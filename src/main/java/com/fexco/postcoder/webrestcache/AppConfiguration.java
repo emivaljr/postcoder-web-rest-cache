9f06fa81a47df26830320c11fe5b397587f28594
Emival Luiz da Silva Junior <emival@gmail.com>
2016-09-17 11:01:14 -0300

    Spring Boot initial implementation finished. This commit resolves #1

---
+++ src/main/java/com/fexco/postcoder/webrestcache/AppConfiguration.java
@@ -0,0 +1,11 @@
+package com.fexco.postcoder.webrestcache;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by emival on 17/09/16.
+ */
+@Configuration
+public class AppConfiguration {
+
+}
