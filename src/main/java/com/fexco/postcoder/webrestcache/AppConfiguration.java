cad70220f62cf6fb162070e280f212303573f00e
Emival Luiz da Silva Junior <emival@gmail.com>
2016-09-17 22:19:38 -0300

    Initial infinispan cache configuration, that resolves #2

--- src/main/java/com/fexco/postcoder/webrestcache/AppConfiguration.java
+++ src/main/java/com/fexco/postcoder/webrestcache/AppConfiguration.java
@@ -1,11 +1,13 @@
 package com.fexco.postcoder.webrestcache;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by emival on 17/09/16.
  */
 @Configuration
+@EnableCaching
 public class AppConfiguration {
 
 }
