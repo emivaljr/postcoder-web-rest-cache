5d0b7921788376c75a7df428c41dc61ce131cf49
Emival Luiz da Silva Junior <emival@gmail.com>
2016-09-19 21:37:38 -0300

    #10:Implementation of client HTML/javascript

--- src/main/java/com/fexco/postcoder/webrestcache/infra/log/LogAspect.java
+++ src/main/java/com/fexco/postcoder/webrestcache/infra/log/LogAspect.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
-
 /**
  * Created by emival on 19/09/16.
  */
@@ -19,9 +17,9 @@ public class LogAspect {
     @Around("execution(* com.fexco.postcoder.webrestcache.restapi.*.*(..)) || execution(* com.fexco.postcoder.webrestcache.integration.*.*(..))")
     public Object logAdvice(ProceedingJoinPoint joinPoint) throws Throwable{
         Logger log = LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringType());
-        log.info(joinPoint.getSignature().getName()+" - begin ");
+        log.info("Method:"+joinPoint.getSignature().getName()+" - begin ");
         Object result = joinPoint.proceed();
-        log.info(joinPoint.getSignature().getName()+" - end ");
+        log.info("Method:"+joinPoint.getSignature().getName()+" - end ");
         return result;
     }
 }
---
+++ src/main/resources/public/client/js/client.js
@@ -0,0 +1,49 @@
+$("#button1id").click(function () {
+  var urlServer = $("#serveraddressinput").val()+'/'+$('input[name=radio-lookup-service]:checked').val()+'/'+$('input[name=radio-address-type]:checked').val();
+  if($('input[name=radio-lookup-service]:checked').val() == 'rgeo'){
+    urlServer += '/'+$("#latitudeinput").val()+'/'+$("#longitudeinput").val();
+  }else{
+      urlServer += '/'+$("#searchaddressinput").val();
+  }
+  var fields =['lines','include','exclude','identifier','distance'];
+  var paramJson = {};
+  for (var i = 0; i < fields.length; i++) {
+    var field = fields[i];
+    if($("#"+field+"input").val() !== ''){
+      paramJson[field] = $("#"+field+"input").val();
+    }
+  }
+  paramJson['format'] = $('input[name=radio-result-format]:checked').val();
+  $.ajax({
+      url: urlServer,
+      data: paramJson
+  }).then(function(data) {
+    $('#result').html('');
+    $('<div/>').text(data).appendTo('#result');
+  });
+});
+$('input[type=radio][name=radio-address-type]').on('change', function() {
+  $("input[name=radio-lookup-service][value='address']").prop("checked",true);
+  $(".div-rgeo").css("display","none");
+  $(".div-search-address").css("display","");
+     switch($(this).val()) {
+         case 'uk':
+             $(".radio-irish-service").css("display","none");
+             break;
+         case 'ie':
+             $(".radio-irish-service").css("display","");
+             break;
+     }
+});
+$('input[type=radio][name=radio-lookup-service]').on('change', function() {
+     switch($(this).val()) {
+         case 'rgeo':
+             $(".div-rgeo").css("display","");
+             $(".div-search-address").css("display","none");
+             break;
+         default:
+             $(".div-rgeo").css("display","none");
+             $(".div-search-address").css("display","");
+             break;
+     }
+});
